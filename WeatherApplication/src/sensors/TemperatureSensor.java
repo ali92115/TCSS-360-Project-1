43f4cd21c8dbde42ced2a1edfd80934aee0a896b
bem4 <38186701+bem4@users.noreply.github.com>
2020-04-07 14:02:51 -0700

    Initial Implementation of the Temperature Sensor
    - Incorporated the minimum and maximum allowed values of the temperature readings according to the specs.
    - Adjusted the format of the temperature readings according to the "resolution" as mentioned in the specs.
    - Changes to the inner and outer temperatures are done by changing each of the temperatures by some random number in the interval [-1, 1) and then making sure each of the temperatures are in the allowed bounds.

--- WeatherApplication/src/sensors/TemperatureSensor.java
+++ WeatherApplication/src/sensors/TemperatureSensor.java
@@ -3,41 +3,121 @@ package sensors;
 import java.text.DecimalFormat;
 
 /**
- * This class is simulating a very basic simulation of temperature and displays its data
- * using the getData method of our interface.
- * @author Ali Iftakhar
+ * This class simulates a temperature sensor.
+ * 
+ * @author Benjamin Munoz
  * @version 4/5/2020
  */
 
-//Feel free to make changes in this class. This is only a basic class written to demonstrate how the 
-//sensor classes should be structured. However, make sure that the final result is provided in getData()
-//since our main class will call this method. You can change how the random data is derived.
 public class TemperatureSensor implements SensorInterface {
-	
+    /**
+     * The minimum possible temperature, in Fahrenheit, that the 
+     * console can display for the inside of the house.
+     * Derived from the specifications.
+     */
+    private static final double INNER_TEMP_MIN_RANGE = 32;
+    
+    /**
+     * The maximum possible temperature, in Fahrenheit, that the 
+     * console can display for the outside of the house.
+     */
+    private static final double INNER_TEMP_MAX_RANGE = 140;
+    
+    /**
+     * The minimum possible temperature, in Fahrenheit, that the 
+     * console can display for the inside of the house.
+     */
+    private static final double OUTER_TEMP_MIN_RANGE = -40;
+    
+    /**
+     * The maximum possible temperature, in Fahrenheit, that the 
+     * console can display for the outside of the house.
+     */
+    private static final double OUTER_TEMP_MAX_RANGE = 150;
+    
+    /**
+     * The format of the temperature values. This in accordance with the 
+     * given limits for the inner and outer temperatures as well as the 
+     * listed "resolution" of the data, which is 0.1°F. 
+     */
+    private static final DecimalFormat TEMP_FORMAT = new DecimalFormat("###.#°F");
+    
 	/**
-	 * Keeps track of the temperature inside the house.
+	 * The temperature inside the house (in degrees Fahrenheit)
 	 */
 	private double temperatureIn;
+	
 	/**
-	 * Keeps track of temperature outside the house.
+	 * The temperature outside the house (in degrees Fahrenheit)
 	 */
 	private double temperatureOut;
 	
+	/**
+	 * The default constructor. The sensor starts with an inside 
+	 * temperature of 75°F and an outside temperature of 
+	 * 42°F. 
+	 */
 	public TemperatureSensor() {
-		temperatureIn = 75; //random value chosen based on realistic assumptions.
-		temperatureOut = 42; 
+		this(75, 42);
 	}
 	
 	/**
-	 * This method must be implemented. This method will be the one the main class uses
-	 * to get an updated value.
+	 * The primary constructor. It allows the sensor to be set to 
+	 * some initial temperatures (in °F).
+	 * 
+	 * An IllegalArgument Exception will be thrown under the following
+	 * conditions:
+	 * <ul>
+	 *     <li>tIn is less than 32°F or greater than 140°F</li>
+	 *     <li>tOut is less than -40°F or greater than 150°F</li>
+	 * </ul>
+	 * 
+	 * @param tIn - The initial inner temperature
+	 * @param tOut - The initial outer temperature
+	 */
+	public TemperatureSensor(double tIn, double tOut) {
+	    if (tIn < INNER_TEMP_MIN_RANGE || tIn > INNER_TEMP_MAX_RANGE) {
+	        String errString = "The inner temperature can only be between ";
+	        errString = errString + INNER_TEMP_MIN_RANGE + " and " + INNER_TEMP_MAX_RANGE;
+	        errString = errString + " degrees Fahrenheit";
+	        throw new IllegalArgumentException(errString);
+	    }
+	    
+	    if (tOut < OUTER_TEMP_MIN_RANGE || tOut > OUTER_TEMP_MAX_RANGE) {
+	        String errString = "The outer temperature can only be between ";
+	        errString = errString + OUTER_TEMP_MIN_RANGE + " and " + OUTER_TEMP_MAX_RANGE;
+	        errString = errString + " degrees Fahrenheit";
+	        throw new IllegalArgumentException(errString);
+	    }
+	    
+	    temperatureIn = tIn;
+	    temperatureOut = tOut;
+	}
+	
+	/**
+	 * Returns the string representing the temperature, in degrees Fahrenheit,
+	 * inside and outside of the house.
 	 */
 	public String getData() {
-		temperatureOut = temperatureOut + Math.random() - Math.random();
-		temperatureIn = temperatureIn + Math.random() - Math.random();
-		//Giving structure and keeping answer to 2 decimal places.
-		return new DecimalFormat("##.##").format(temperatureOut) + "\n" +  
-				new DecimalFormat("##.##").format(temperatureIn);
+		temperatureOut += (Math.random() * 2) - 1;
+		temperatureIn += (Math.random() * 2) - 1;
+		
+		if (temperatureIn < INNER_TEMP_MIN_RANGE) {
+		    temperatureIn = INNER_TEMP_MIN_RANGE;
+		} else if (temperatureIn > INNER_TEMP_MAX_RANGE) {
+		    temperatureIn = INNER_TEMP_MAX_RANGE;
+		}
+		
+		if (temperatureOut < OUTER_TEMP_MIN_RANGE) {
+		    temperatureOut = OUTER_TEMP_MIN_RANGE;
+		} else if (temperatureOut > OUTER_TEMP_MAX_RANGE) {
+		    temperatureOut = OUTER_TEMP_MAX_RANGE;
+		}
+		
+		String outerTempString = TEMP_FORMAT.format(temperatureOut);
+		String innerTempString = TEMP_FORMAT.format(temperatureIn);
+		
+		return "{outsideTemperature: " + outerTempString 
+		        + ", insideTemperature: " + innerTempString + "}";  
 	}
 }
-
